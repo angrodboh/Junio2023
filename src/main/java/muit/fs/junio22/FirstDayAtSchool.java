f4f841a9180773cfa48422f1f7de840adee12f70
angrodboh <smartnexus@icloud.com>
2023-06-01 12:02:58 +0200

    Corregido desc test constructor, se esperan 3 elementos (no 2)

--- src/main/java/muit/fs/junio22/FirstDayAtSchool.java
+++ src/main/java/muit/fs/junio22/FirstDayAtSchool.java
@@ -1,10 +1,10 @@
 /**
- * Clase a probar en el código ejemplo para el control de junio de 2022
+ * Clase a probar en el cï¿½digo ejemplo para el control de junio de 2022
  */
 package muit.fs.junio22;
 
 /**
- * @author Isabel Román
+ * @author Isabel Romï¿½n
  *
  */
 
@@ -23,7 +23,7 @@ public class FirstDayAtSchool {
 	public void prepareMyBag() {
 		schoolbag.put("Pens", 1);
 		schoolbag.put("Notebooks", 1);
-                schoolbag.put("Sharpener", 1);
+        schoolbag.put("Sharpener", 1);
 	}
 
 	public void addThings(String thingName, int number) {
